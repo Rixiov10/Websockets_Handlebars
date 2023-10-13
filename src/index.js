459e742f7cb71f4bd75713fe547cc07fbecb9315
Rixiov10 <rixiov10@gmail.com>
2023-10-12 23:41:35 -0300

    home y saludo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express, {urlencoded} from "express"
 import {engine} from "express-handlebars"
 import __dirname from "./utils.js"
-
+import * as path from "path" 
 
 const app = express();
 const PORT = 4000;
@@ -11,11 +11,13 @@ app.use(urlencoded({ extended: true}));
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars")
-
+app.set("views", path.resolve(__dirname + "/views") );
 
 app.get("/", (req, res) => {
-    res.send("Hola soy un servidor por express");
-})
+    res.render("home", {
+        title: "Backend | Handlebars"
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`Sevidor por puerto ${PORT}`);
